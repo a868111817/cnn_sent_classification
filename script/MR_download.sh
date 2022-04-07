9872f664d630ec63d7d4ff1b27823f74c255f850
a868111817 <a868111817@gmail.com>
2022-04-07 12:46:30 +0800

    script

---
+++ script/MR_download.sh
@@ -0,0 +1,4 @@
+# Download Datasets
+wget -P 'data/' 'https://www.cs.cornell.edu/people/pabo/movie-review-data/rt-polaritydata.tar.gz'
+# Unzip
+tar xvzf 'data/rt-polaritydata.tar.gz' -C 'data/'
---
+++ script/fasttext_download.sh
@@ -0,0 +1,3 @@
+wget -P fastText "https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip"
+
+unzip fastText/crawl-300d-2M.vec.zip -d fastText
